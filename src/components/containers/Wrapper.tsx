3fe69d4f253585cfb986fada24405662b51a8654
Jesus Varela <jesusaugusto.008@gmail.com>
2022-06-06 16:26:30 -0400

    (feat): add cherry blossom border on main wrapper

--- src/components/containers/Wrapper.tsx
+++ src/components/containers/Wrapper.tsx
@@ -1,13 +1,20 @@
 import React, { ReactNode } from 'react'
 import styled from 'styled-components'
+import Mark from '../../assets/flowermark.png'
 
 const WrapperContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  border: 5px solid red;
+  height: calc(100vh - 50px);
+  border: 25px solid pink;
+  -moz-border-image: url(${Mark});
+  -webkit-border-image: url(${Mark});
+  -o-border-image: url(${Mark});
+  border-image: url(${Mark});
+  border-image-slice: 180;
+  border-image-repeat: round;
 `
 
 interface WrapperProps {
